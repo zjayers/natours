1617bc3e01c3744266e2bac695e53c1b54ce842a
Zjayers <z.ayers@icloud.com>
2020-03-11 13:22:47 -0500

    After Refactor

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+exports.getAllUsers = (request, response) => {
+  response
+    .status(500)
+    .json({ status: 'error', message: 'This route is note yet defined' });
+};
+
+exports.getUser = (request, response) => {
+  response
+    .status(500)
+    .json({ status: 'error', message: 'This route is note yet defined' });
+};
+
+exports.createUser = (request, response) => {
+  response
+    .status(500)
+    .json({ status: 'error', message: 'This route is note yet defined' });
+};
+
+exports.updateUser = (request, response) => {
+  response
+    .status(500)
+    .json({ status: 'error', message: 'This route is note yet defined' });
+};
+
+exports.deleteUser = (request, response) => {
+  response
+    .status(500)
+    .json({ status: 'error', message: 'This route is note yet defined' });
+};
