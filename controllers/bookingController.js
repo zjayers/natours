a01f4247ea43202df594692150ff40fabfbe9dfe
Zach Ayers <z.ayers@icloud.com>
2020-03-20 16:09:41 -0500

    Project Completion

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -50,7 +50,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
 
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
